e287f8a4b3ac4c3d3bce1944fa98086aa807e495
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2016-11-12 12:34:17 +0100

    move unpacking of deploy key up, before we clone

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,20 @@ MESSAGE=`git log -1 --pretty=%B`
 AUTHOR=`git log -1 --pretty=%an`
 EMAIL=`git log -1 --pretty=%ae`
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 # Clone the existing gh-pages into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+echo "git clone $REPO out"
 git clone $REPO out
 
 # Clean out existing contents
@@ -40,17 +52,6 @@ fi
 # The delta will show diffs between new and old versions.
 git add .
 git commit -m "originally ${SHA}: ${MESSAGE}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO
